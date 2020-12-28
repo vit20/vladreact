3ce7e5981657279c0ca891282867c5756427472a
Witek <witeku777@gmail.com>
2020-12-28 08:56:54 +0100

    finitto

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,20 +1,31 @@
 import React, {useEffect} from 'react'
 import TodoList from './Todo/TodoList'
 import Context from './context'
-import AddTodo from './Todo/AddTodo'
+import Loader from './Loader'
+import Modal from './Modal/Modal'
 
+
+const AddTodo = React.lazy(() => new Promise(resolve =>{
+setTimeout(()=>{
+  resolve(import("./Todo/AddTodo"))
+},3000)
+}))
 
 
 
 function App() {
   const [todos, setTodos] = React.useState([])
+  const[loading, setLoading] = React.useState(true)
+
+
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
     .then(response => response.json())
     .then(todos => {
       setTodos (todos)
-    })
+      setLoading(false)
+    },2000)
   }, [])
 
 
@@ -42,15 +53,22 @@ function addTodo(title) {
     <Context.Provider value={{removeTodo}}>
   <div className="wrapper">
     <h1>Tutorial</h1>
+
+    <Modal/>
+
+
+    <React.Suspense fallback={<p>Loading...</p>}>
     <AddTodo onCreate={addTodo}/>
+    </React.Suspense>
+    
+
+    {loading && <Loader/>}
 
 {todos.length ? (
 <TodoList todos={todos} onToggle={toggleTodo}/>
-): (
+): loading ? null : ( 
 <p>No tudos!</p>
 )}
-
-    
   </div>
   </Context.Provider>
   
